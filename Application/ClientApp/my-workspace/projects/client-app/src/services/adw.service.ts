819e15b7f72a996de0ecd5bc906785c1ef9cf976
Maxim T <BBGONE@users.noreply.github.com>
2024-07-08 21:33:20 +0300

    added DataVew example code

--- Application/ClientApp/my-workspace/projects/client-app/src/services/adw.service.ts
+++ Application/ClientApp/my-workspace/projects/client-app/src/services/adw.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Optional, SkipSelf } from '@angular/core';
-import { IPromise, IQueryResult, IStatefulPromise, Utils, IRowData, IColumn } from 'jriapp-lib';
-import { DbContext, Product } from "../db/adwDB";
+import {
+  IPromise, IQueryResult, IStatefulPromise, Utils,
+  IRowData, IColumn, DataView, BaseObject
+} from 'jriapp-lib';
+import { DbContext, Product, ProductCategory, ProductCategoryDb } from "../db/adwDB";
 
 const utils = Utils;
 
@@ -21,6 +24,64 @@ type IStaticData = {
   productCategoryData: IQueryResponse;
 };
 
+export class Filter extends BaseObject {
+  readonly dbContext: DbContext;
+
+  private _parentCategoryId: number = null;
+  private _childCategoryId: number = null;
+  private _parentCategories: DataView<ProductCategory>;
+  private _childCategories: DataView<ProductCategory>;
+
+  constructor(dbContext: DbContext) {
+    super();
+    this.dbContext = dbContext;
+    const self = this;
+
+    //filters top level product categories
+    this._parentCategories = new DataView<ProductCategory>(
+      {
+        dataSource: this.dbContext.dbSets.ProductCategory,
+        fn_sort: function (a, b) { return a.ProductCategoryId - b.ProductCategoryId; },
+        fn_filter: function (item) { return item.ParentProductCategoryId === null; }
+      });
+
+    //filters product categories which have parent category
+    this._childCategories = new DataView<ProductCategory>(
+      {
+        dataSource: this.dbContext.dbSets.ProductCategory,
+        fn_sort: function (a, b) { return a.ProductCategoryId - b.ProductCategoryId; },
+        fn_filter: function (item) { return item.ParentProductCategoryId !== null && item.ParentProductCategoryId == self.parentCategoryId; }
+      });
+  }
+
+  reset() {
+    this.parentCategoryId = null;
+    this.childCategoryId = null;
+  }
+
+  get parentCategoryId() {  return this._parentCategoryId;  }
+  set parentCategoryId(v) {
+    if (this._parentCategoryId != v) {
+      this._parentCategoryId = v;
+      this.objEvents.raiseProp('parentCategoryId');
+      this._childCategories.syncRefresh();
+    }
+  }
+  get childCategoryId() { return this._childCategoryId; }
+  set childCategoryId(v) {
+    if (this._childCategoryId != v) {
+      this._childCategoryId = v;
+      this.objEvents.raiseProp('childCategoryId');
+    }
+  }
+
+  get dbSets() { return this.dbContext.dbSets; }
+  get parentCategories() { return this._parentCategories; }
+  get childCategories() { return this._childCategories; }
+  get productModels() { return this.dbSets.ProductModel; }
+  get productCategories() { return this.dbSets.ProductCategory; }
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -28,8 +89,8 @@ export class AdwService {
   private _dbContext: DbContext;
   private _uniqueID: string;
   private _initPromise: Promise<any>;
+  private _filter: Filter = null;
 
-  
   constructor(
     @Optional() @SkipSelf() existingService: AdwService,
     private readonly http: HttpClient
@@ -67,6 +128,11 @@ export class AdwService {
   }
 
   async loadStaticData() {
+    const self = this;
+
+    this._filter = new Filter(this.dbContext);
+   
+
     const url = this.dbContext.getUrl('static');
     const promise = new Promise<IStaticData>((resolve, reject) => {
       this.http.get(url).subscribe(
@@ -75,10 +141,22 @@ export class AdwService {
       );
     });
     const staticData = await promise;
+
+    // subscribe to the view refreshed event
+    this.filter.parentCategories.addOnViewRefreshed((_s, args) => {
+      if (this.filter.parentCategories.currentItem) {
+        do {
+          const parentItem = this.filter.parentCategories.currentItem;
+          this.filter.parentCategoryId = parentItem.ProductCategoryId;
+
+          console.log(`parentCategory: ${this.filter.parentCategoryId}) ${parentItem.Name}`, this.filter.childCategories.items);
+        } while (this.filter.parentCategories.moveNext());
+      }
+
+    }, this.uniqueID);
+
     this.dbContext.dbSets.ProductModel.fillData(staticData.productModelData);
     this.dbContext.dbSets.ProductCategory.fillData(staticData.productCategoryData);
-
-    console.log("productModel", this.dbContext.dbSets.ProductModel.items);
   }
 
   load(pageIndex: number = 0, pageSize: number): IStatefulPromise<IQueryResult<Product>> {
@@ -133,5 +211,8 @@ export class AdwService {
   }
   get uniqueID() {
     return this._uniqueID;
+  }
+  get filter() {
+    return this._filter;
   }
 }
